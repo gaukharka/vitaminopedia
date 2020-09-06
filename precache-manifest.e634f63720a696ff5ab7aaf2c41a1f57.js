58721f973cccf5167556d42997affaa5a07ed447
gaukharka <gaukhar.kashaganova@gmail.com>
2020-09-06 11:30:52 +0200

    Updates

---
+++ precache-manifest.e634f63720a696ff5ab7aaf2c41a1f57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d2826496f347a66162a07143f97cd7",
+    "url": "/vitaminopedia/index.html"
+  },
+  {
+    "revision": "d65f9523bcec83cb3f61",
+    "url": "/vitaminopedia/static/js/2.76fca339.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vitaminopedia/static/js/2.76fca339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34c0c0a981e8843de8a",
+    "url": "/vitaminopedia/static/js/main.65c619be.chunk.js"
+  },
+  {
+    "revision": "58cfad04fd41381c72d6",
+    "url": "/vitaminopedia/static/js/runtime-main.b33604f0.js"
+  }
+]);
